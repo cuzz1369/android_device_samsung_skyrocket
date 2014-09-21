mnkaz/android_device_samsung_celoxdcm

skyrocket: Clean up Samsung device inheritance
* Remove init.qcom.usb.sh
* Move torch package up to msm8660-common
* Move common items from device to celox-common
* Use nested cm.dependencies
* Move common system properties to celox-common, msm8660-common

Change-Id: Ifb0a1457032c8a87dbddb3c10f7ce2543ffbf714

BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,51 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#
-# This file sets variables that control the way modules are built
-# thorughout the system. It should not be used to conditionally
-# disable makefiles (the proper mechanism to control what gets
-# included in a build is to use PRODUCT_PACKAGES in a product
-# definition file).
-#
-
-# WARNING: This line must come *before* including the proprietary
-# variant, so that it gets overwritten by the parent (which goes
-# against the traditional rules of inheritance).
-USE_CAMERA_STUB := true
-
 # inherit from common celox
 -include device/samsung/celox-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/samsung/skyrocket/BoardConfigVendor.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := MSM8660_SURF
-
 # Assert
+TARGET_BOARD_INFO_FILE ?= device/samsung/skyrocket/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := SGH-I727,skyrocket
 
 # Kernel
-TARGET_KERNEL_CONFIG        := cyanogenmod_skyrocket_defconfig
-TARGET_KERNEL_SOURCE        := kernel/samsung/msm8660-common
+TARGET_KERNEL_CONFIG := cyanogenmod_skyrocket_defconfig
 
-# Assert minimum baseband version
-TARGET_BOARD_INFO_FILE ?= device/samsung/skyrocket/board-info.txt
-
-# cat /proc/emmc
-#dev:        size     erasesize name
-#mmcblk0p22: 00fffc00 00000200 "recovery"
-#mmcblk0p8: 01000000 00000200 "boot"
-#mmcblk0p24: 5ffffc00 00000200 "system"
-#mmcblk0p26: 13fffe00 00000200 "cache"
-#mmcblk0p25: 9ffffe00 00000200 "userdata"
-
-TARGET_USERIMAGES_USE_EXT4 := true
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776192
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 838860800
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 20044333056
-BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
 
+# Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/skyrocket/bluetooth
-

cuzz1369/android_device_samsung_skyrocket

BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,51 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#
-# This file sets variables that control the way modules are built
-# thorughout the system. It should not be used to conditionally
-# disable makefiles (the proper mechanism to control what gets
-# included in a build is to use PRODUCT_PACKAGES in a product
-# definition file).
-#
-
-# WARNING: This line must come *before* including the proprietary
-# variant, so that it gets overwritten by the parent (which goes
-# against the traditional rules of inheritance).
-USE_CAMERA_STUB := true
-
 # inherit from common celox
 -include device/samsung/celox-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/samsung/skyrocket/BoardConfigVendor.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := MSM8660_SURF
-
 # Assert
+TARGET_BOARD_INFO_FILE ?= device/samsung/skyrocket/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := SGH-I727,skyrocket
 
 # Kernel
-TARGET_KERNEL_CONFIG        := Sultan_defconfig
-TARGET_KERNEL_SOURCE        := kernel/samsung/skyrocket
+TARGET_KERNEL_CONFIG := Sultan_defconfig
 
-# Assert minimum baseband version
-TARGET_BOARD_INFO_FILE ?= device/samsung/skyrocket/board-info.txt
-
-# cat /proc/emmc
-#dev:        size     erasesize name
-#mmcblk0p22: 00fffc00 00000200 "recovery"
-#mmcblk0p8: 01000000 00000200 "boot"
-#mmcblk0p24: 5ffffc00 00000200 "system"
-#mmcblk0p26: 13fffe00 00000200 "cache"
-#mmcblk0p25: 9ffffe00 00000200 "userdata"
-
-TARGET_USERIMAGES_USE_EXT4 := true
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776192
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 838860800
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 20044333056
-BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
 
+# Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/skyrocket/bluetooth
-